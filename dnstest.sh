d78d1da8cd0ffab26531bbb8693893fced202a36
Vladimir Skok <skok@vova.io>
2020-12-21 20:13:02 -0500

    refactor: update dnstest

--- dnstest.sh
+++ dnstest.sh
@@ -1,63 +1,54 @@
 #!/usr/bin/env bash
 
-command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
-{ command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
+NAMESERVER=127.0.0.1
+RESULT=result.txt
+LIST=test.txt
+DIVIDER='==========================================================================='
 
+command -v bc > /dev/null ||
+{ echo "bc was not found. Please install bc."; exit 1; }
 
+{ command -v drill > /dev/null && dig=drill; } ||
+{ command -v dig > /dev/null && dig=dig; } ||
+{ echo "dig was not found. Please install dnsutils."; exit 1; }
 
-NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
+# List of domains to test against
+source "${LIST}"
 
-PROVIDERS="
-1.1.1.1#cloudflare 
-4.2.2.1#level3 
-8.8.8.8#google 
-9.9.9.9#quad9 
-80.80.80.80#freenom 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
-"
-
-# Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
-
-
-totaldomains=0
-printf "%-18s" ""
-for d in $DOMAINS2TEST; do
-    totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
-done
-printf "%-8s" "Average"
-echo ""
-
-
-for p in $NAMESERVERS $PROVIDERS; do
-    pip=${p%%#*}
-    pname=${p##*#}
-    ftime=0
-
-    printf "%-18s" "$pname"
-    for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
-        if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000
-        elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
-	    fi
-
-        printf "%-8s" "$ttime ms"
-        ftime=$((ftime + ttime))
-    done
-    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
-
-    echo "  $avg"
+domain_count=0
+# Get total number of domains
+for i in "${!DOMAIN_LIST[@]}"; do
+    domain_count=$((i + 1))
 done
 
+printf "\n${DIVIDER}\n" 2>&1 | tee -a ${RESULT}
+printf "$NAMESERVER" 2>&1 | tee -a ${RESULT}
+printf "\n${DIVIDER}\n" 2>&1 | tee -a ${RESULT}
+
+total_time=0
+
+for i in "${!DOMAIN_LIST[@]}"; do
+    domain=${DOMAIN_LIST[$i]}
+    index=$(($i+1))
+
+    time=$(dig +tries=1 +time=2 +stats @$NAMESERVER "$domain" | grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2)
+    if [ -z "$time" ]; then
+        # Timeout 1s = 1000ms
+        time=1000
+    elif [ "x$time" = "x0" ]; then
+        time=1
+    fi
+
+    printf "\n$index. $domain $time ms" 2>&1 | tee -a ${RESULT}
+    total_time=$((total_time + time))
+done
+
+average_time=$(bc -lq <<< "scale=2; $total_time/$domain_count")
+
+printf "\n\n${DIVIDER}\n" 2>&1 | tee -a ${RESULT}
+printf "TOTAL NUMBER OF DOMAINS: $domain_count\n" 2>&1 | tee -a ${RESULT}
+printf "TOTAL TIME: $total_time ms\n" 2>&1 | tee -a ${RESULT}
+printf "AVERAGE: $average_time ms\n" 2>&1 | tee -a ${RESULT}
+printf "${DIVIDER}\n" 2>&1 | tee -a ${RESULT}
 
 exit 0;
